f8ebbce4d4c4b2eea8176821a8d6abc6131cea0d
Ziqian Li <liziqian1992@gmail.com>
2022-06-07 22:08:16 -0700

    set up database, complete add-item rount

---
+++ database/index.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const url = "mongodb://localhost:27017/rainbow";
+mongoose.connect(url, { useNewUrlParser: true });
+
+// check DB connection
+const db = mongoose.connection;
+db.once('open', _ => {
+  console.log('Database connected:', url);
+})
+
+db.on('error', err => {
+  console.error('connection error:', err);
+})
+
+const rainbowSchema = new Schema({
+  name: String,
+  color: String,
+  category: String,
+  count: Number
+})
+
+const rainbow = mongoose.model('Rainbow', rainbowSchema);
+
+// save a new entry to DB
+module.exports.save = (entry) => {
+  return rainbow.updateOne(entry, entry, { upsert: true }).exec();
+}
+
+// retrieve either ONE entry that matches OR ALL entries in DB
+module.exports.fetch = (entry) => {
+  if (entry) {
+    return rainbow.find({ name: entry.name }).exec();
+  } else {
+    return rainbow.find({}).exec();
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,31 +1,58 @@
 const express = require("express");
 const path = require("path");
+const db = require("../database/index.js");
 
 var exampleData = require("../example-data.json");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "/../client")));
-app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/index", (req, res) => {
+app.get('/index', (req, res) => {
   // get all veggie/fruit data from DB
   res.status(200).json(exampleData);
 });
 
-app.post("/add-item", (req, res) => {
+app.post('/add-item', (req, res) => {
   // handle post request for adding veggie/fruit w/ color, inital count set to 0
-  // add item to DB
-  // if duplicate is found, count ++
-    // after successfully added it to DB, get all data from DB
-      // response with the data
 
   console.log(req.body);
-  exampleData = exampleData.push(req.body);
-  res.status(201);
+
+  var entry = req.body;
+
+  // convert all data to lowercase
+  for(var k in entry) {
+    entry[k] = entry[k].toLowerCase();
+  }
+
+  // search DB to check duplicate entry
+  db.fetch(entry)
+    .then(result => {
+      // check if entry exists in DB
+      if (result.length !== 0) {
+        res.status(406).send('Error: already exsit');
+      } else {
+        entry.count = 0;
+        // save entry to DB
+        db.save(entry)
+          .then(result => {
+            console.log('added successfully', result);
+            res.status(201).send(result);
+          })
+          .catch(err => {
+            res.status(500).send('unable to save entry to database. Error message: ' + err);
+          })
+      }
+    })
+    .catch(err => {
+      res.status(500).send('fail to fetch entry. Error message: ' + err);
+    })
+
+  // MAKE SURE ERRORS ARE PASSED TO THE CLIENT
 })
 
-app.post("/click-item", (req, res) => {
+app.post('/click-item', (req, res) => {
   // handle post request for adding count on veggie/fruit
 })
 

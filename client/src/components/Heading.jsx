1de265648a6a30990a9ffb244f197ec3eaeb6bac
Ziqian Li <liziqian1992@gmail.com>
2022-06-09 22:40:32 -0700

    copy change, set up a new reset button in Heading component

--- client/src/components/Heading.jsx
+++ client/src/components/Heading.jsx
@@ -23,11 +23,12 @@ function Heading(props) {
   return (
     <div>
       <h3>One color at a time, paint your plate with veggies and fruits!</h3>
-      <p>Here's the rule:</p>
-      <p>When you eat 1 serving of vegetable or fruit, click it to add. The tracker shows how you're doing so far.</p>
-      <p>1 serving is 1/2 cup chopped vegetable, 1 cup leafy vegetable, 1/2 cup fruit, 1 small piece of fruit.</p>
+      <p>Rule #1: When you eat 1 serving of vegetable or fruit, click it to add. The tracker shows how you're doing so far.</p>
+      <p>Rule #2: 1 serving is 1/2 cup chopped vegetable, 1 cup leafy vegetable, 1/2 cup fruit, 1 small piece of fruit.</p>
       <p>Enjoy the challenge and the boost of your health!</p>
-      <h3>Rainbow-meter</h3>
+      <h3>Rainbow Meter</h3>
+      {/* create button to restart a week */}
+      <button></button>
       <h4>{colorMessage}</h4>
       <h4>{totalMessage}</h4>
     </div>

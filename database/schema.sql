8c54de4bd78572fc07f402cae9b0ab8e8a451f32
Ziqian Li <liziqian1992@gmail.com>
2022-08-24 13:42:27 -0700

    add data import to schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,10 @@
-DROP TABLE IF EXISTS rainbow_food
-CREATE TABLE rainbow_food (
+DROP TABLE IF EXISTS rainbow_foods;
+CREATE TABLE rainbow_foods (
   id SERIAL,
-  name TEXT,
+  name TEXT CONSTRAINT name_unique UNIQUE,
   color TEXT,
   category TEXT,
   count INTEGER
 );
+
+COPY rainbow_foods (name, category,count,color) FROM '/Users/ziqianli/Documents/HackReactor/hr-rpp36-mvp/database/rainbow-food.csv' DELIMITER ',' CSV header;
